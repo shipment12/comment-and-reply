45ea74e81cb3e3c5d160472ee8b1f81a27ec91c8
udedibor favour <udedborfavour@gmail.com>
2019-09-02 10:28:33 +0100

    created relationship in question, reply, like, user

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -1,10 +1,24 @@
 <?php
 
 namespace App\Model;
+use App\User;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function questions()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
